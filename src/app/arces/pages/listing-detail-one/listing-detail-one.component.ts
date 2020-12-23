527c03368f0d8815c38b4ea92f03d5fd3e9f14d6
jignaseth407 <jignaseth407@gmail.com>
2020-12-23 11:57:31 +0530

    first commit

---
+++ src/app/arces/pages/listing-detail-one/listing-detail-one.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-detail-one',
+  templateUrl: './listing-detail-one.component.html',
+  styleUrls: ['./listing-detail-one.component.css']
+})
+export class ListingDetailOneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/arces/pages/submit-listing/submit-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-listing',
+  templateUrl: './submit-listing.component.html',
+  styleUrls: ['./submit-listing.component.css']
+})
+export class SubmitListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
